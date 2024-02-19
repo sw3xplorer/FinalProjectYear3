3767b5cc70886e097731ee64440dd4bd8076af18
Robert Popa <robert.popa@elev.ga.ntig.se>
2024-02-19 13:34:57 +0100

    First commit

---
+++ FinalProjectYear3/Bullet.cs
@@ -0,0 +1,13 @@
+﻿namespace FinalProjectYear3;
+
+public class Bullet
+{
+    protected string _name;
+    protected int _damage;
+    protected int _size;
+
+    public virtual void Hit()
+    {
+        
+    }
+}
