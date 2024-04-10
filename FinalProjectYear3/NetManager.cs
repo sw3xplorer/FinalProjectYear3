2363163803b8be324141cdf60d4328e19622267b
Robert Popa <robert.popa@elev.ga.ntig.se>
2024-04-10 14:50:09 +0200

    Network functional

--- FinalProjectYear3/NetManager.cs
+++ FinalProjectYear3/NetManager.cs
@@ -15,31 +15,51 @@ public static class NetManager
 
     public static void Client()
     {
-        _connectedClient = new TcpClient("127.0.0.1", 1234);
-        Listen();
+        try
+        {
+            _connectedClient = new TcpClient("127.0.0.1", 1234);
+            Listen();
+        }
+        catch
+        {
+            Console.WriteLine("Failed to connect. Enter to retry:");
+            Console.ReadLine();
+        }
     }
 
     // Host stuff
 
     public static void Host()
     {
+
         _listener = new TcpListener(IPAddress.Any, 1234);
         _listener.Start();
         // Program freezes until someone connects.
         _connectedClient = _listener.AcceptTcpClient();
+        Listen();
     }
 
-static Thread listenThread = new(() => {
+    static Thread listenThread = new(() =>
+    {
         _stream = _connectedClient.GetStream();
-        while(_connectedClient.Connected)
+        while (_connectedClient.Connected)
         {
             // Buffer contains data that was received
             byte[] _buffer = new byte[_connectedClient.ReceiveBufferSize];
-            _stream.Read(_buffer);
+            try
+            {
+                _stream.Read(_buffer);
+            }
+            catch
+            {
+                Console.WriteLine("Host disconnected. Exiting...");
+                Task.Delay(2000).Wait();
+                Environment.Exit(0);
+            }
             // BitConverter.ToBoolean(_buffer[0]);
             bool _hitResult = _buffer[0] > 0;
             int _damageResult = BitConverter.ToInt32(_buffer[1..]);
-            if(_hitResult)
+            if (_hitResult)
             {
                 Console.WriteLine($"Got hit for: {_damageResult}");
             }
@@ -48,7 +68,7 @@ static Thread listenThread = new(() => {
                 Console.WriteLine("The bullet missed. Hit for: 0");
             }
         }
-});
+    });
 
     public static void Listen()
     {
@@ -63,7 +83,33 @@ static Thread listenThread = new(() => {
         List<byte> _bytes = new();
         _bytes.AddRange(BitConverter.GetBytes(hit));
         _bytes.AddRange(BitConverter.GetBytes(damage));
-        _stream.Write(_bytes.ToArray());
+        if(_stream != null)
+        {
+            _stream.Write(_bytes.ToArray());
+        }
     }
-
+    public static string DecideHost()
+    {
+        Console.WriteLine("1 for host or 2 for client.");
+        string _result = Console.ReadLine();
+        while (_result != "1" || _result != "2")
+        {
+            if (_result == "1")
+            {
+                Host();
+                break;
+            }
+            else if (_result == "2")
+            {
+                Client();
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Write 1 or 2.");
+                _result = Console.ReadLine();
+            }
+        }
+        return _result;
+    }
 }
--- FinalProjectYear3/Program.cs
+++ FinalProjectYear3/Program.cs
@@ -1,20 +1,5 @@
 ﻿using FinalProjectYear3;
 using System.Net;
-Console.WriteLine("1 for host or 2 for client.");
-string _result = Console.ReadLine();
-if(_result == "1")
-{
-    NetManager.Host();
-}
-else if(_result == "2")
-{
-    NetManager.Client();
-}
-else
-{
-
-}
-
 Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
 
 BulletCollection _bulletCollection = new();
@@ -23,8 +8,14 @@ _9mm _9Mm = new();
 _45ACP _45ACP = new();
 _50Cal _50Cal = new();
 Gun _gun = new();
-
-_gun.UserControl();
+if(NetManager.DecideHost() == "1")
+{
+    _gun.UserControl();
+}
+else
+{
+    Target.DrawTarget();
+}
 
 Console.WriteLine("Mag fully emptied!");
 Console.ReadLine();
